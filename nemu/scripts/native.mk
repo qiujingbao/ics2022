e2d44a0a4f860d579b59054f3229996fdcaf3415
tracer-ics2022 <tracer@njuics.org>
2023-07-08 13:45:19 +0000

    >  run NEMU 211220000 张三 Linux ubuntu 5.15.0-1040-azure #47~20.04.1-Ubuntu SMP Fri Jun 2 21:38:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:45:19 up 10 days,  6:05,  0 users,  load average: 1.06, 0.47, 0.23

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,7 +24,7 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
-override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt --batch
+override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
