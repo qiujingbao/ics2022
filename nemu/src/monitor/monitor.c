565b023ac81d5432946933c6a075306bd0d591f4
tracer-ics2022 <tracer@njuics.org>
2023-07-08 13:39:01 +0000

    >  compile NEMU 211220000 张三 Linux ubuntu 5.15.0-1040-azure #47~20.04.1-Ubuntu SMP Fri Jun 2 21:38:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:39:01 up 10 days,  5:59,  0 users,  load average: 0.15, 0.12, 0.09

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -69,7 +69,6 @@ static long load_img() {
 }
 
 static int parse_args(int argc, char *argv[]) {
-  printf("%s\n",argv[0]);
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
