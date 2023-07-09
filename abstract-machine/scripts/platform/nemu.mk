52060871ad8813349c01d1fa05e40c4fdeb9c8da
tracer-ics2022 <tracer@njuics.org>
2023-07-09 06:28:20 +0000

    >  compile NEMU 211220000 张三 Linux ubuntu 5.15.0-1040-azure #47~20.04.1-Ubuntu SMP Fri Jun 2 21:38:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:28:20 up 10 days, 22:48,  0 users,  load average: 0.07, 0.07, 0.04

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 #一键回归测试 加上-b参数
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt --elf=$(IMAGE) -b
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt --elf=$(IMAGE).elf -b
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
