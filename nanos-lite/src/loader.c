be30183270ae9c7482e55ac198bbdc0d6c25651e
tracer-ics2022 <tracer@njuics.org>
2023-07-12 16:59:30 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:30 up  3:47,  0 users,  load average: 0.54, 0.45, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,22 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len) ;
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len) ;
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,39 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
+#include <proc.h>
+/* 0是系统调用号 1 2 3分别是第x个参数  */
+static uintptr_t args[4];
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+int sys_exit()
+{
+  Log("%d %p",args[1],args[1]);
+  halt((int)args[1]);
+  return 1;
+}
+int sys_yield()
+{
+  yield();
+  return 1;
+}
+
+/* steal from xv6 os */
+static int (*syscalls[])(void) = {
+    [SYS_yield] sys_yield,
+    [SYS_exit] sys_exit,
+};
+void do_syscall(Context *c)
+{
+  args[0] = c->GPR1;
+  args[1] = c->GPR2;
+  args[2] = c->GPR3;
+  args[3] = c->GPR4;
+  if (0 <= args[0] && args[0] <= 20 && syscalls[args[0]])
+  {
+    /* do sys call*/
+    c->GPRx = syscalls[args[0]]();
+  }
+  else
+  {
+    panic("Unhandled syscall ID = %d", args[0]);
   }
 }
