b31d5c32495c6ba4cfa4ee3850b171666e778d12
tracer-ics2022 <tracer@njuics.org>
2023-07-12 21:17:42 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:42 up  8:05,  0 users,  load average: 0.56, 0.42, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,16 +3,19 @@
 #include <fs.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-   Elf_Ehdr elf;
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  Log("loader:%s", filename);
+  Elf_Ehdr elf;
   int fd = fs_open(filename);
+  assert(fd != -1);
   fs_lseek(fd, 0, SEEK_SET);
   fs_read(fd, &elf, sizeof(Elf_Ehdr));
   Elf_Phdr Phdr;
@@ -48,9 +51,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   return ehdr.e_entry;*/
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
