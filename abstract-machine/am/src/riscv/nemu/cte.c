f616748ef9ca983c572db2686bc79929389aac09
tracer-ics2022 <tracer@njuics.org>
2023-07-12 17:03:58 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:58 up  3:51,  0 users,  load average: 0.65, 0.50, 0.38

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,9 +7,17 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->mcause) {
-      case -1: ev.event= EVENT_YIELD;break;
-      default: ev.event = EVENT_ERROR; break;
+    if (c->mcause >= 0 && c->mcause < 20)
+    {
+      ev.event = EVENT_SYSCALL;
+    }
+    else if (c->mcause == -1)
+    {
+      ev.event = EVENT_YIELD;
+    }
+    else
+    {
+      ev.event = EVENT_ERROR;
     }
 
     c = user_handler(ev, c);
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,8 +1,9 @@
 #include <common.h>
-
+extern void do_syscall(Context*);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: Log("yield be called"); break;
+    case EVENT_SYSCALL:do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
