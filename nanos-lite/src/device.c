7d5132906a306b16e67c2595addf54fff73644a7
tracer-ics2022 <tracer@njuics.org>
2023-07-13 20:46:45 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:45 up  5:36,  1 user,  load average: 0.71, 0.40, 0.30

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
+  yield();
   for (int i = 0; i < len; i++)
   {
     putch(((char *)buf)[i]);
@@ -42,6 +43,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 //TODO:添加对len的处理
 size_t events_read(void *buf, size_t offset, size_t len) {
+  yield();
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if(ev.keycode==AM_KEY_NONE)return 0;
   sprintf(buf,"%s %s\n",ev.keydown?"kd":"ku",keyname[ev.keycode]);
