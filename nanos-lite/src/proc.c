6b1f42c9002cecb9363f41001e1664272892889a
tracer-ics2022 <tracer@njuics.org>
2023-07-13 21:20:32 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:32 up  6:09,  1 user,  load average: 0.27, 0.24, 0.26

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -11,6 +11,7 @@ void switch_boot_pcb() {
 }
 
 void hello_fun(void *arg) {
+  printf("a\n");
   int j = 1;
   while (1) {
     Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (uintptr_t)arg, j);
@@ -30,9 +31,10 @@ extern void naive_uload(PCB *pcb, const char *filename);
 extern void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 static char *args_menu[] = {"/bin/menu", "sss","ok",NULL};
 void init_proc() {
-   context_uload(&pcb[0], "/bin/hello", args_menu,NULL);
+  context_kload(&pcb[0], hello_fun,NULL);
+   context_uload(&pcb[1], "/bin/hello", args_menu,NULL);
 
-  context_kload(&pcb[1], hello_fun,NULL);
+  
 
   switch_boot_pcb();
   Log("Initializing processes...");
