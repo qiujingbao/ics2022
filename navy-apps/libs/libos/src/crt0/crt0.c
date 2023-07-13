cf6c44b10e9be98d40f4b88363acffea123d228c
tracer-ics2022 <tracer@njuics.org>
2023-07-13 21:11:13 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:13 up  6:00,  1 user,  load average: 0.55, 0.31, 0.28

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include <stdio.h>
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
