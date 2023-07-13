cdfa669cbbb0ce6a717f9ae1e411e61d9f646cf9
tracer-ics2022 <tracer@njuics.org>
2023-07-13 21:10:39 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:39 up  6:00,  1 user,  load average: 0.20, 0.22, 0.25

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,6 +9,10 @@ void call_main(uintptr_t *args) {
   char **argv = (char **)args + 1; // 解析出 argv
   char **envp = ((char **)argv+argc+1);
   environ = envp;
+  for(int i=0;i<argc;i++)
+  {
+    printf("%s\n",argv[i]);
+  }
   exit(main(argc, argv, envp));
   assert(0);
 }
