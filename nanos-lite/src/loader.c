4817439ba80932adaa0aa5a9f41a79eca29ea714
tracer-ics2022 <tracer@njuics.org>
2023-07-13 20:35:42 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:42 up  5:25,  1 user,  load average: 0.30, 0.30, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -63,5 +63,5 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   uintptr_t entry = loader(pcb, filename);
   pcb->cp = ucontext(&pcb->as, heap, (void *)entry);
   pcb->cp->GPRx = (uintptr_t)heap.end;
-  Log("entry:%p gprx:%p cp:%p",entry,pcb->cp->GPRx,pcb->cp);
+  //Log("entry:%p gprx:%p cp:%p",entry,pcb->cp->GPRx,pcb->cp);
 }
