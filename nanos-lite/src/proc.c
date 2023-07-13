642f0d8a8b76635f35017c0fe498f7c53173954b
tracer-ics2022 <tracer@njuics.org>
2023-07-13 16:34:56 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:56 up  1:24,  1 user,  load average: 0.14, 0.16, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
   }
@@ -24,9 +24,11 @@ void context_kload(PCB *create_pcb, void (*entry)(void *), void *arg)
   Area stack = {create_pcb->stack, create_pcb->stack + STACK_SIZE};
   create_pcb->cp = kcontext(stack, entry, arg);
 }
+char *hello_arg_test="kcontext !!!";
 extern void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, NULL);
+  context_kload(&pcb[0], hello_fun, hello_arg_test);
+  context_kload(&pcb[1], hello_fun, hello_arg_test);
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -39,7 +41,7 @@ Context *schedule(Context *prev)
 current->cp = prev;
 
 // always select pcb[0] as the new process
-current = &pcb[0];
+current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
 // then return the new context
 return current->cp;
