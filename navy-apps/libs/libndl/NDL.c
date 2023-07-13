9c9d35ebd1a80228649bf06e85c12dfa9dc58c78
tracer-ics2022 <tracer@njuics.org>
2023-07-13 10:26:47 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:26:47 up  9:24,  1 user,  load average: 0.38, 0.35, 0.31

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -9,86 +9,111 @@ static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 static int canvas_w = 0, canvas_h = 0;
-static int fbctr,fb;
-
-uint32_t NDL_GetTicks() {
+static int fbctr, fb;
+#define HAS_CENTER 1
+uint32_t NDL_GetTicks()
+{
   struct timeval tv;
   struct timezone tz;
-  gettimeofday(&tv, &tz);  
-  return tv.tv_usec / 1000 + 1000 * tv.tv_sec;//ms
+  gettimeofday(&tv, &tz);
+  return tv.tv_usec / 1000 + 1000 * tv.tv_sec; // ms
 }
-int NDL_PollEvent(char *buf, int len) {
-  int fd=open("/dev/events",0,0);
-  return read(fd,buf,len);
+int NDL_PollEvent(char *buf, int len)
+{
+  int fd = open("/dev/events", 0, 0);
+  return read(fd, buf, len);
 }
 
-void NDL_OpenCanvas(int *w, int *h) {
-//猜测用于加载app，暂时忽略
-  if (getenv("NWM_APP")) {
+void NDL_OpenCanvas(int *w, int *h)
+{
+  // 猜测用于加载app，暂时忽略
+  if (getenv("NWM_APP"))
+  {
     int fbctl = 4;
     fbdev = 5;
-    screen_w = *w; screen_h = *h;
+    screen_w = *w;
+    screen_h = *h;
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
     write(fbctl, buf, len);
-    while (1) {
+    while (1)
+    {
       // 3 = evtdev
       int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
+      if (nread <= 0)
+        continue;
       buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
+      if (strcmp(buf, "mmap ok") == 0)
+        break;
     }
     close(fbctl);
   }
-  else{
-    if(*w==0&&*h==0){
+  else
+  {
+    if (*w == 0 && *h == 0)
+    {
       char buf[64];
-      read(fbctr,buf,64);
-      sscanf(buf,"WIDTH:%d\nHEIGHT:%d\n",&canvas_w,&canvas_h);
-      *w=canvas_w;*h=canvas_h;
+      read(fbctr, buf, 64);
+      sscanf(buf, "WIDTH:%d\nHEIGHT:%d\n", &canvas_w, &canvas_h);
+      *w = canvas_w;
+      *h = canvas_h;
     }
-    else{
-      canvas_w=*w,canvas_h=*h;
+    else
+    {
+      canvas_w = *w, canvas_h = *h;
     }
-    printf("w=%d\th=%d\n",canvas_w,canvas_h);
+    printf("w=%d\th=%d\n", canvas_w, canvas_h);
   }
 }
 
-void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  for (int i = 0; i < h; i++) {
-    lseek(fb, ((y + i) * screen_w + x) * 4, SEEK_SET); 
-    write(fb,(pixels + i * w), w * 4);
+void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
+{
+#ifdef HAS_CENTER
+  x = (canvas_w / 2 - w / 2);
+  y = (canvas_h / 2 - h / 2);
+#endif
+  for (int i = 0; i < h; i++)
+  {
+    lseek(fb, ((y + i) * screen_w + x) * 4, SEEK_SET);
+    write(fb, (pixels + i * w), w * 4);
   }
 }
 
-void NDL_OpenAudio(int freq, int channels, int samples) {
+void NDL_OpenAudio(int freq, int channels, int samples)
+{
 }
 
-void NDL_CloseAudio() {
+void NDL_CloseAudio()
+{
 }
 
-int NDL_PlayAudio(void *buf, int len) {
+int NDL_PlayAudio(void *buf, int len)
+{
   return 0;
 }
 
-int NDL_QueryAudio() {
+int NDL_QueryAudio()
+{
   return 0;
 }
 
-int NDL_Init(uint32_t flags) {
-  if (getenv("NWM_APP")) {
+int NDL_Init(uint32_t flags)
+{
+  if (getenv("NWM_APP"))
+  {
     evtdev = 3;
   }
   char buf[64];
-  fbctr=open("/proc/dispinfo",0,0);
-  fb=open("/dev/fb",2,0);
-  //printf("%d\n",fbctr);
-  read(fbctr,buf,64);
-  sscanf(buf,"WIDTH:%d\nHEIGHT:%d\n",&screen_w,&screen_h);
-  
+  fbctr = open("/proc/dispinfo", 0, 0);
+  fb = open("/dev/fb", 2, 0);
+  // printf("%d\n",fbctr);
+  read(fbctr, buf, 64);
+  sscanf(buf, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
+
   return 0;
 }
 
-void NDL_Quit() {
+void NDL_Quit()
+{
 }
