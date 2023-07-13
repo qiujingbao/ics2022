ad34439a873ed0f5d77279733c8bd8a62f58a9c6
tracer-ics2022 <tracer@njuics.org>
2023-07-13 11:21:30 +0800

    >  compile NEMU 211220000 张三 Linux qiujingbao-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:30 up 29 min,  1 user,  load average: 0.40, 0.45, 0.53

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -5,15 +5,20 @@
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
 
-SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
-  assert(src->type == RW_TYPE_MEM);
-  assert(freesrc == 0);
-  return NULL;
+SDL_Surface* IMG_Load(const char *filename) {
+  printf("image load:%s\n",filename);
+  FILE *fp=fopen(filename,"r");
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  char *buf = malloc(size);
+  fread(buf, 1, size, fp);
+  SDL_Surface *s = STBIMG_LoadFromMemory(buf, size);
+  fclose(fp);
+  free(buf);
+  return s;
 }
 
-SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
-}
 
 int IMG_isPNG(SDL_RWops *src) {
   return 0;
